39387e53b2bfb9c2d47e50a936625e92ffff42bf
Wendy Calderon <wendycalderon@gmail.com>
2018-08-16 02:54:15 +0000

    Add all possible attr to Coffee Class

--- lib/todays_coffee_cli/coffee.rb
+++ lib/todays_coffee_cli/coffee.rb
@@ -1,3 +1,20 @@
 class Coffee::CLI
+  attr_accessor :name, :prep_time, :ingredients, :description, :instructions 
   
+  def self.coffee_types
+    coffee_all = [] 
+    deals << self.scrape_cafepoint
+    deals
+  end
+  
+  def self.scrape_cafepoint
+    doc = Nokogiri::HTML(open("http://www.cafepoint.co.uk/different-types-of-coffee/"))
+    coffee = self.new
+    coffee.name =
+    coffee.prep_time =
+    coffee.ingredients = 
+    coffee.description =
+    coffee.instructions =
+    coffee_all
+  end
 end
