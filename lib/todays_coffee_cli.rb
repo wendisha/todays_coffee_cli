015152b09dfb3b0cb1b10e55bbf18be3a04758bc
Wendy Calderon <wendycalderon@gmail.com>
2018-08-16 16:06:56 +0000

    Working on dependencies

--- lib/todays_coffee_cli.rb
+++ lib/todays_coffee_cli.rb
@@ -1,8 +1,10 @@
 #File to access enviroment
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "./todays_coffee_cli/version"
 require_relative './todays_coffee_cli/coffee'
 require_relative './todays_coffee_cli/cli'
 
-require 'open-uri'
-require 'nokogiri'
-require 'pry'
+
