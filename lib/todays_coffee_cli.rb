409112805777491a519e6666d4e1aa20f32fe5b0
Wendy Calderon <wendycalderon@gmail.com>
2018-08-16 03:02:16 +0000

    Add dependencies

--- lib/todays_coffee_cli.rb
+++ lib/todays_coffee_cli.rb
@@ -1,3 +1,8 @@
 #File to access enviroment
 require_relative "./todays_coffee_cli/version"
+require_relative './todays_coffee_cli/coffee'
 require_relative './todays_coffee_cli/cli'
+
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
